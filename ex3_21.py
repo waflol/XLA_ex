f50b54925c8c5df1245946986de00c015fee98a6
waflol <lenguyenhunganh2001@gmail.com>
2022-05-28 01:58:18 +0700

    A

--- ex3_21.py
+++ ex3_21.py
@@ -4,77 +4,87 @@ import cv2
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
+from torchvision.transforms import GaussianBlur
+
+def pyrDown(tensor_img):
+    gauss_blur = GaussianBlur(kernel_size=(5,5),sigma=(1/256.,36/256.))
+    gauss_img = gauss_blur(tensor_img)
+    # gauss_img = cv2.GaussianBlur(tensor_img[0].T.numpy(),(5,5),0)
+    down_sampling = nn.Upsample(scale_factor=0.5,mode='bilinear')
+    # return down_sampling(torch.tensor([gauss_img.T]))
+    return down_sampling(gauss_img)
+
+def pyrUp(tensor_img):
+    up_sampling = nn.Upsample(scale_factor=2,mode='bilinear')
+    return up_sampling(tensor_img)
+
+alpha = 1.5
+
 A = cv2.imread('data/apple.jpg')
 A = cv2.resize(A,(256,256))
+
 B = cv2.imread('data/orange.jpg')
 B = cv2.resize(B,(256,256))
 
-# class DoubleConv(nn.Module):
-#     """(convolution => [BN] => ReLU) * 2"""
+A = A/255.
+B = B/255.
 
-#     def __init__(self, in_channels, out_channels, mid_channels=None):
-#         super().__init__()
-#         if not mid_channels:
-#             mid_channels = out_channels
-#         self.double_conv = nn.Sequential(
-#             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-#             nn.BatchNorm2d(mid_channels),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU(inplace=True)
-#         )
+tensor_A = torch.tensor([A.T])
+tensor_B = torch.tensor([B.T])
 
-#     def forward(self, x):
-#         return self.double_conv(x)
+# generate Gaussian pyramid for A
+G = tensor_A.clone()
+gpA = [G]
+for i in range(6):
+    G = pyrDown(G)
+    gpA.append(G)
     
-# class Down(nn.Module):
-#     """Downscaling with maxpool then double conv"""
-
-#     def __init__(self, in_channels, out_channels):
-#         super().__init__()
-#         self.maxpool_conv = nn.Sequential(
-#             nn.MaxPool2d(2),
-#             DoubleConv(in_channels, out_channels)
-#         )
-
-#     def forward(self, x):
-#         return self.maxpool_conv(x)
+# generate Gaussian pyramid for B
+G = tensor_B.clone()
+gpB = [G]
+for i in range(6):
+    G = pyrDown(G)
+    gpB.append(G)
 
 
-# class Up(nn.Module):
-#     """Upscaling then double conv"""
+# generate Laplacian Pyramid for A
+lpA = [gpA[5]]
+for i in range(5,0,-1):
+    GE = pyrUp(gpA[i])
+    L = torch.subtract(gpA[i-1],GE)
+    lpA.append(L)
+# generate Laplacian Pyramid for B
+lpB = [gpB[5]]
+for i in range(5,0,-1):
+    GE = pyrUp(gpB[i])
+    L = torch.subtract(gpB[i-1],GE)
+    lpB.append(L)
+    
 
-#     def __init__(self, in_channels, out_channels, bilinear=True):
-#         super().__init__()
+# Now add left and right halves of images in each level
+LS = []
+for la,lb in zip(lpA,lpB):
+    _,dpt,rows,cols = la.shape
+    ls = torch.concat((la[:,:,0:cols//2,:], lb[:,:,cols//2:,:]),dim=2)
+    LS.append(ls)
+    
+# now reconstruct
+ls_ = LS[0]
+for i in range(1,6):
+    ls_ = pyrUp(ls_)
+    ls_ = torch.add(ls_, alpha*LS[i])
+    
+real = torch.concat((tensor_A[:,:,0:cols//2,:],tensor_B[:,:,cols//2:,:]),dim=2)
 
-#         # if bilinear, use the normal convolutions to reduce the number of channels
-#         if bilinear:
-#             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-#             self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-#         else:
-#             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-#             self.conv = DoubleConv(in_channels, out_channels)
+plt.figure(figsize=(16,16))
+plt.subplot(121)
+plt.title('Pyramid_blending_pytorch')
+plt.imshow(ls_[0].T.numpy()[:,:,-1::-1])
+plt.axis('off')
 
-#     def forward(self, x1, x2):
-#         x1 = self.up(x1)
-#         # input is CHW
-#         diffY = x2.size()[2] - x1.size()[2]
-#         diffX = x2.size()[3] - x1.size()[3]
-
-#         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-#                         diffY // 2, diffY - diffY // 2])
-#         # if you have padding issues, see
-#         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-#         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-#         x = torch.cat([x2, x1], dim=1)
-#         return self.conv(x)
-
-
-# class OutConv(nn.Module):
-#     def __init__(self, in_channels, out_channels):
-#         super(OutConv, self).__init__()
-#         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-
-#     def forward(self, x):
-#         return self.conv(x)
+plt.subplot(122)
+plt.title('Direct_blending')
+plt.imshow(real[0].T.numpy()[:,:,-1::-1])
+plt.axis('off')
+plt.savefig('data/Pyramidblending_result_ex3_21.jpg')
+plt.show()
